2c8d4bd30120d4888edcf83004350a81c415f13f
Shyshkin Konstantin <shyshkin.konstantin@gmail.com>
2025-01-02 10:55:30 +0000

    vectors

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,37 @@
-
-struct Book {
-    title: String,
-    pub author: String,
-    pub pages: u32,
-}
-
-fn print_book(book :&Book) {
-    println!("Title: {}, Author: {}, Pages: {}",
-            book.title,
-            book.author,
-            book.pages);
-}
-
-fn set_title(book :&mut Book, title :String) {
-    book.title = title;
-}
-
-fn add_to_title(book :&mut Book, extra :String) {
-    let formated_extra = format!(" ({})", extra);
-    book.title.push_str(&formated_extra);
-}
-
 fn main() {
-    let mut book = Book {
-        title: String::from("Rust"),
-        author: String::from("James Gosling"),
-        pages: 100,
-    };
-    // print_book(&book);
-    // set_title(&mut book, "Rust Programming".to_string());
-    // print_book(&book);
-    // add_to_title(&mut book, "Best Book Ever".to_string());
-    // print_book(&book);
-    book.title = "Rust Programming".to_string();
-    print_book(&book);
+  // let mut v: Vec<i32> = Vec::new();
+  // v.push(10);
+  // v.push(20);
+  // v.push(30);
+
+  let v = vec![10,20,30];
+
+  println!("first elem {}", v[0]);
+
+  match v.get(3) {
+    Some(value) => println!("second elem {}", value),
+    None => println!("Non elem found"),
+  }
+
+  let mut books_v: Vec<String> = Vec::new();
+
+  books_v.push("Help".to_string());
+  books_v.push("Me".to_string());
+
+  println!("first book: {}", books_v[0]);
+
+  match books_v.get(1) {
+    Some(value) => println!("second: {}", value),
+    None => println!("To DO"),
+  }
+
+  for value in &mut books_v {
+    *value += "_changed";
+  }
+
+  books_v.pop();
+  books_v.pop();
+  books_v.remove(0);
+  
+  println!("{:?}", books_v);
 }
